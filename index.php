fa769272ebb37c1c3e39618c8b8b78a6e4f37fbc
SupriyaKJadhav2244 <supriyakj3707@gmail.com>
2021-09-20 18:12:55 +0530

    My Code

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!--JQuery-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<!-- <script type="text/javascript" src="jquery-3.3.1.min.js"></script> -->
+	<!--Floating WhatsApp css-->
+	<!-- <link rel="stylesheet" href="floating-wpp.min.css"> -->
+	<!--Floating WhatsApp javascript-->
+	<!-- <script type="text/javascript" src="floating-wpp.min.js"></script> -->
+
+</head>
+<style type="text/css">
+.fa-whatsapp  {
+  color:#fff;
+  background:
+  linear-gradient(#25d366,#25d366) no-repeat,
+  radial-gradient(#25d366 60%,transparent 0);
+}
+.fa-twitter  {
+   color: skyblue;
+    text-shadow: 1px 1px 1px #ccc;
+    font-size: 1.5em;
+}
+.fa-facebook  {
+   color: blue;
+    text-shadow: 1px 1px 1px #ccc;
+    font-size: 1.5em;
+}
+.fa-instagram  {
+   color: #C60950;
+    text-shadow: 1px 1px 1px #ccc;
+    font-size: 1.5em;
+}
+.fa-envelope  {
+   color: #7DC010;
+    text-shadow: 1px 1px 1px #ccc;
+    font-size: 1.5em;
+}
+
+.fa-phone  {
+   color: #69B92D;
+    text-shadow: 1px 1px 1px #ccc;
+    font-size: 1.5em;
+} 
+.fa-map-marker {
+   color: red;
+    text-shadow: 1px 1px 1px #ccc;
+    font-size: 1.5em;
+} 
+</style>
+<body> 
+
+<!--Div where the WhatsApp will be rendered-->
+<!-- <div id="WAButton"></div> -->
+<!-- <script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script> --> 
+
+<span>
+	<i class="fa fa-whatsapp" style="font-size:25px;"></i>
+	<i class="fa fa-twitter" aria-hidden="true"></i>
+	<a href =""> 
+		<i class="fa fa-facebook" aria-hidden="true"> 
+		</i>
+	</a> 
+	 
+	</i>
+	<a href =""> 
+		<i class="fa fa-instagram" aria-hidden="true"> 
+		</i>
+	</a>
+	
+	<i class="fa fa-envelope" aria-hidden="true"></i>
+	<i class="fa fa-phone" aria-hidden="true"></i>
+	<i class="fa fa-map-marker" aria-hidden="true"></i>
+</span> <br> 
+	
+		<form class="" action="">
+		  <input type="text" placeholder="Search.." name="search">
+		  <button type="submit"><i class="fa fa-search"></i>Search</button>
+		</form><br> 
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3785.8639125843974!2d76.58631941436698!3d18.39904377768293!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcf83a208654909%3A0x2e897e3ffac589af!2sShahu%20Chowk%2C%20Anand%20Nagar%2C%20Latur%2C%20Maharashtra%20413512!5e0!3m2!1sen!2sin!4v1601370140964!5m2!1sen!2sin" width="1000" height="600" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+</body>
+</html>
